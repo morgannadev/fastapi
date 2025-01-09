89927e7fb5eb14006c202a2f991b395d3c2f0832
morgannadev <morgcontato@gmail.com>
2025-01-09 17:44:16 -0300

    teste retorno html

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo'}
+
+
+@app.get('/ola', status_code=200, response_class=HTMLResponse)
+def ola_mundo():
+    return """
+    <html>
+    <head>
+        <title>Olá mundo</title>
+    </head>
+    <body>
+        <h1>Olá mundo</h1>
+    </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,24 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo'}
+
+
+def test_ola_mundo_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/ola')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+    <head>
+        <title>Olá mundo</title>
+    </head>
+    <body>
+        <h1>Olá mundo</h1>
+    </body>
+    </html>
+    """
+    )
