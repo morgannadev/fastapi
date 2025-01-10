a52b0204c6b7b9e6192f16dfd49d537d14959c38
morgannadev <morgcontato@gmail.com>
2025-01-10 19:05:05 -0300

    Adiciona o updated_at para Users

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
@@ -5,9 +8,6 @@ from sqlalchemy.orm import Session
 
 from fast_zero.app import app
 from fast_zero.models import table_registry
-
-from contextlib import contextmanager
-from datetime import datetime
 
 
 @pytest.fixture
@@ -31,6 +31,9 @@ def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
+
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
 
     event.listen(model, 'before_insert', fake_time_hook)
 
